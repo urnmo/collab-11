e48ba54337351956672dc9962fa52be2a32acab1
Scott Endicott <sendicott1391@gmail.com>
2016-08-19 16:26:41 -0400

    Refined initial GetInfo function

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
-function receivedMessage () {
-    console.log("received Message");
+let currentValue = 0;
+
+function getInfo () {
     let request = new XMLHttpRequest();
     request.open('GET', 'http://chat.queencityiron.com/messages');
     request.addEventListener('load', function () {
-        let delivery = JSON.parse(request.responseText);
-        console.log(delivery);
-        let userCollection = delivery.collection; // not final location
+        let response = JSON.parse(request.responseText);
+
+        // currentValue = response.length;
+        for (let i = currentValue; currentValue < response.length; i++) {
+            if (response.length > i) {
+                // currentValue = response.length;
+            }
+        }
+
+        currentValue = response.length;
+
+        console.log(response);
+        let userCollection = response.collection; // not final location
         let userString = userCollection.user; // not final location
         let userText = userCollection.message; // not final location
 
-        let parent = document.querySelector('body');
-        let messageBoard = document.createElement('section');
-        parent.appendChild(messageBoard);
-
-        for (let i = 0; i < userCollection.length; i++) {
-            let receivedUN = document.createElement('p');
-            receivedUN.textContent = userString[i];
-            let receivedText = document.createElement('p');
-            receivedText.textContent = userText[i];
-            messageBoard.appendChild(receivedUN + ": " + receivedText);
-        }
+        // for (let i = 0; i < userCollection.length; i++) {
+        //     let receivedUN = document.createElement('p');
+        //     receivedUN.textContent = userString[i];
+        //     let receivedText = document.createElement('p');
+        //     receivedText.textContent = userText[i];
+        //     let messageBoard = document.querySelector('section');
+        //     messageBoard.appendChild(receivedUN + ": " + receivedText);
+        // }
     });
     request.send();
 }
 
 
 window.addEventListener('load', function () {
-    // setInterval(receivedMessage, 3000);
-    receivedMessage();
+    // setInterval(getInfo, 3000);
+    getInfo();
 });
