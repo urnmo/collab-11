2e2b5535c0173c618124f41bd236098e0f3a4488
Scott Endicott <sendicott1391@gmail.com>
2016-08-20 11:54:12 -0400

    Defined append received messages function

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
 let currentValue = 0;
 
+function appendMessages(x) {
+    let received = document.createElement('p');
+    received.textContent = x;
+    let messageBoard = document.querySelector('section');
+    messageBoard.appendChild(x);
+    x.classList.add('incoming');
+}
+
+
+
+
 function getInfo () {
     let request = new XMLHttpRequest();
     request.open('GET', 'http://chat.queencityiron.com/messages');
     request.addEventListener('load', function () {
         let response = JSON.parse(request.responseText);
-
-        // currentValue = response.length;
+        console.log(response);
         for (let i = currentValue; currentValue < response.length; i++) {
-            if (response.length > i) {
-                // currentValue = response.length;
+            if (i < response.length) {
+                let newMessage = response[i].user + ": " + response[i].message;
+                appendMessages(newMessage);
             }
         }
 
         currentValue = response.length;
-
-        console.log(response);
-        let userCollection = response.collection; // not final location
-        let userString = userCollection.user; // not final location
-        let userText = userCollection.message; // not final location
-
-        // for (let i = 0; i < userCollection.length; i++) {
-        //     let receivedUN = document.createElement('p');
-        //     receivedUN.textContent = userString[i];
-        //     let receivedText = document.createElement('p');
-        //     receivedText.textContent = userText[i];
-        //     let messageBoard = document.querySelector('section');
-        //     messageBoard.appendChild(receivedUN + ": " + receivedText);
-        // }
+        
     });
     request.send();
 }
