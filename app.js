03385eac82dbe33a41a412d4bf6cba178b06570f
Scott Endicott <sendicott1391@gmail.com>
2016-08-20 18:47:01 -0400

    Appended sent message successfully

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 let currentValue = 0;
 
+
+// Questions for this function:
+// 1. How can it identify whether the message coming in is
+//  being received or being sent?
 function appendMessages(x) {
-    let received = document.createElement('p');
-    received.textContent = x;
     let messageBoard = document.getElementById('messageBoard');
-    messageBoard.appendChild(received);
-    received.classList.add('incoming');
+    messageBoard.appendChild(x);
     console.log(x);
 }
 
@@ -18,29 +19,52 @@ function getInfo () {
         for (let i = currentValue; i < response.length; i++) {
             if (i < response.length) {
                 let newMessage = response[i].user + ": " + response[i].message;
-                appendMessages(newMessage);
+                let received = document.createElement('p');
+                received.textContent = newMessage;
+                received.classList.add('incoming');
+                appendMessages(received);
             }
         }
         currentValue = response.length;
-        
     });
     request.send();
 }
 
-let newInput = document.getElementById("message");
-let currentName = document.getElementById("username");
-let theButton = document.querySelector("button");
-
-function sentMessage() {
-    let sentMsg = currentName.value + " " + newInput.value;
-    appendMessages(sentMsg);
+function addMessage(msg) {
+    let request = new XMLHttpRequest();
+    request.open('POST', 'http://chat.queencityiron.com/messages');
+    request.addEventListener('load', function () {
+        getInfo();
+    });
+    request.send(JSON.stringify(msg));
 }
 
-theButton.addEventListener('click', function () {
-    sentMessage();
+window.addEventListener('load', function () {
+    getInfo();
+
+    let submitBtn = document.getElementById("theButton");
+    submitBtn.addEventListener('click', function () {
+        addMessage({
+            user: document.getElementById('username').value,
+            message: document.getElementById('message').value,
+        });
+    });
 });
 
-window.addEventListener('load', function () {
-    // setInterval(getInfo, 3000);
-    getInfo();
-});
+// let newInput = document.getElementById("message");
+// let currentName = document.getElementById("username");
+// let theButton = document.querySelector("button");
+
+// function sentMessage() {
+//     let sentMsg = currentName.value + " " + newInput.value;
+//     appendMessages(sentMsg);
+// }
+
+// // theButton.addEventListener('click', function () {
+// //     sentMessage();
+// // });
+
+// window.addEventListener('load', function () {
+//     // setInterval(getInfo, 3000);
+//     getInfo();
+// });
